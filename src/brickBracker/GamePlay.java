78f064111ceaca6cf05576a12468c4506ce18466
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-02-13 10:01:33 +0100

    create GamePlay class and graphics

---
+++ src/brickBracker/GamePlay.java
@@ -0,0 +1,69 @@
+package brickBracker;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer timer;
+    private int delay = 8;
+    private int playerX = 310;
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    GamePlay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics graphics) {
+        // background
+        graphics.setColor(Color.black);
+        graphics.fillRect(1, 1, 692, 592);
+
+        // borders
+        graphics.setColor(Color.magenta);
+        graphics.fillRect(0, 0, 3, 592);
+        graphics.fillRect(0, 0, 692, 3);
+        graphics.fillRect(691, 0, 3, 592);
+
+        // the paddle
+        graphics.setColor(Color.green);
+        graphics.fillRect(playerX, 550, 110, 8);
+
+        // the ball
+        graphics.setColor(Color.yellow);
+        graphics.fillOval(ballposX, ballposY, 20, 20);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+}
--- src/brickBracker/Main.java
+++ src/brickBracker/Main.java
@@ -5,6 +5,8 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.add(gamePlay);
         obj.setBounds(10, 10, 700, 600);
         obj.setTitle("Breakout Ball");
         obj.setResizable(false);
