029874d5e4a956c604cb300a391bac8da89ae8bc
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-02-13 11:48:55 +0100

    ball intersection with bricks

--- src/brickBracker/GamePlay.java
+++ src/brickBracker/GamePlay.java
@@ -64,6 +64,38 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
                 ballYdir = -ballYdir;
             }
 
+            for (int i=0; i<map.map.length; i++) {
+                for(int j=0; j<map.map[0].length; j++) {
+                    if(map.map[i][j] > 0) {
+                        int brickX = j * map.brickWidth + 80;
+                        int brickY = i * map.brickHeight + 50;
+                        int brickWidth = map.brickWidth;
+                        int brickHeight = map.brickHeight;
+
+                        Rectangle rect = new Rectangle(
+                                brickX,
+                                brickY,
+                                brickWidth,
+                                brickHeight
+                        );
+
+                        Rectangle ballRect = new Rectangle(
+                                ballposX,
+                                ballposY,
+                                20,
+                                20
+                        );
+                        Rectangle brickRect = rect;
+
+                        if (ballRect.intersects(brickRect)) {
+                            map.setBrickValue(0, i, j);
+                            totalBricks--;
+                            score += 5;
+                        }
+                    }
+                }
+            }
+
             ballposX += ballXdir;
             ballposY += ballYdir;
             if (ballposX < 0) {
--- src/brickBracker/MapGenerator.java
+++ src/brickBracker/MapGenerator.java
@@ -42,4 +42,8 @@ public class MapGenerator {
             }
         }
     }
+
+    public void setBrickValue(int value, int row, int col) {
+        map[row][col] = value;
+    }
 }
